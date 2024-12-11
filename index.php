b1c95aa283823ea61d6e5391d377ebd4054d21a0
fjar86github <84884351+fjar86github@users.noreply.github.com>
2024-12-11 01:17:54 -0700

    Update index.php

--- index.php
+++ index.php
@@ -33,37 +33,42 @@ if ($method == 'OPTIONS') {
     http_response_code(200);
     exit();
 }
-$urlendpoint="/onlinestorev2/index.php";
 
-if (strpos($uri, $urlendpoint.'/api/register') === 0){
+// Path endpoint yang diinginkan
+$baseApiPath = '/index.php/api/';
+
+// Menangani permintaan berdasarkan path yang diterima
+if (strpos($uri, $baseApiPath . 'register') === 0) {
     require_once 'api/register.php';
-} elseif (strpos($uri, $urlendpoint.'/api/login') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'login') === 0) {
     require_once 'api/login.php';
-} elseif (strpos($uri, $urlendpoint.'/api/profile') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'profile') === 0) {
     require_once 'api/profile.php';
-}elseif (strpos($uri, $urlendpoint.'/api/products') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'products') === 0) {
     require_once 'api/products.php';
-}elseif (strpos($uri, $urlendpoint.'/api/cart') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'cart') === 0) {
     require_once 'api/cart.php';
-}elseif (strpos($uri, $urlendpoint.'/api/orders') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'orders') === 0) {
     require_once 'api/orders.php';
-}elseif (strpos($uri, $urlendpoint.'/api/reviews') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'reviews') === 0) {
     require_once 'api/reviews.php';
-}elseif (strpos($uri, $urlendpoint.'/api/categories') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'categories') === 0) {
     require_once 'api/categories.php';
-}elseif (strpos($uri, $urlendpoint.'/api/notifications') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'notifications') === 0) {
     require_once 'api/notifications.php';
-}elseif (strpos($uri, $urlendpoint.'/api/users') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'users') === 0) {
     require_once 'api/users.php';
-}elseif (strpos($uri, $urlendpoint.'/api/transaksi') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'transaksi') === 0) {
     require_once 'api/transaksi.php';
-}elseif (strpos($uri, $urlendpoint.'/api/segmentasipelanggan') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'segmentasipelanggan') === 0) {
     require_once 'api/segmentasipelanggan.php';
-}elseif (strpos($uri, $urlendpoint.'/api/chatbot') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'chatbot') === 0) {
     require_once 'api/chatbot.php';
-}elseif (strpos($uri, $urlendpoint.'/api/mysqlstatus') === 0) {
+} elseif (strpos($uri, $baseApiPath . 'mysqlstatus') === 0) {
     require_once 'api/mysqlstatus.php';
-}else {
+} else {
+    // Jika tidak ditemukan endpoint yang sesuai
     header("HTTP/1.1 404 Not Found");
+    print("base uri=".$uri."<br>");
     echo json_encode(['message' => 'Endpoint not found']);
 }
